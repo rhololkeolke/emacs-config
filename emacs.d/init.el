fcfe551004290994d98ad2b00960fba4df23a39a
Devin Schwab <digidevin@gmail.com>
2017-12-12 16:59:21 -0500

    :sparkles: Added a bunch of org-mode config blocks

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,6 +5,22 @@
 
 ;; load the preload-custom.el
 (setq custom-file (format "%spreload-custom.el" user-emacs-directory))
+(if (not (file-exists-p custom-file))
+    (with-temp-buffer
+      (insert "(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+")
+(write-file custom-file)))
 (load custom-file)
 
 ;; Setup custom group
@@ -79,6 +95,8 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 
