63ab673e41c53054273ffc4065ad793f4ffc8b78
Devin Schwab <dschwab@andrew.cmu.edu>
2017-12-20 19:27:42 -0500

    :sparkles: Proper indentation for FlatBuffers

--- emacs.d/lisp/flatbuffers.el
+++ emacs.d/lisp/flatbuffers.el
@@ -19,14 +19,37 @@
 	(,flatbuffers-types-regexp . font-lock-type-face)
 	(,flatbuffers-keywords-regexp . font-lock-keyword-face)))
 
+(defvar flatbuffers-indent-offset 2
+  "*Indentation offset for `flatbuffers-mode'.")
+
+(defun flatbuffers-indent-line ()
+  "Indent current line for `flatbuffers-mode'."
+  (interactive)
+  (let ((indent-col 0))
+    (save-excursion
+      (beginning-of-line)
+      (condition-case nil
+          (while t
+            (backward-up-list 1)
+            (when (looking-at "[[{]")
+              (setq indent-col (+ indent-col flatbuffers-indent-offset))))
+        (error nil)))
+    (save-excursion
+      (back-to-indentation)
+      (when (and (looking-at "[]}]") (>= indent-col flatbuffers-indent-offset))
+        (setq indent-col (- indent-col flatbuffers-indent-offset))))
+    (indent-line-to indent-col)))
+
 (defvar flatbuffers-mode-hook '())
 
 (define-derived-mode flatbuffers-mode prog-mode "flatbuffers-mode"
   "Major mode for editing Flatbuffer Messages"
   (setq font-lock-defaults '((flatbuffers-font-lock-keywords)))
-  (setq-local indent-line-function 'indent-relative)
-  (setq-local tab-width 2)
-  (setq-local indent-tabs-mode nil))
+  (setq comment-start "// ")
+  (setq comment-end "")
+  (setq indent-line-function 'flatbuffers-indent-line)
+  (setq tab-width flatbuffers-indent-offset)
+  (setq indent-tabs-mode nil))
 
 (provide 'flatbuffers-mode)
 
